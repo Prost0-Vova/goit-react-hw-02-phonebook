71a5ef2cd60cc3175b82d2df1c3414b905f63738
Vova <vovazbuchkin9@gmail.com>
2023-08-02 19:53:06 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,16 +7,29 @@ import Filter from './Filter';
 
 export class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     filter: '',
     name: '',
     number: '',
   };
 
   addContact = (newContact) => {
+    const { contacts } = this.state;
     this.setState((prevState) => ({
       contacts: [...prevState.contacts, { ...newContact, id: nanoid() }],
     }));
+
+
+
+    if (contacts.some((contact) => contact.name === newContact.name)) {
+      alert(`${newContact.name} is already in contacts.`);
+      return;
+    };
   };
 
   deleteContact = (id) => {
@@ -36,6 +49,7 @@ export class App extends Component {
     const filteredContacts = contacts.filter((contact) =>
       contact.name.toLowerCase().includes(filter.toLowerCase())
     );
+ 
 
     return (
       <div>
